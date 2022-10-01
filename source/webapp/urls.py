1c3b0b2d3cd892fa29f7e86ee795d72e51fe19a9
Daniarbekov <daniarbekov@gmail.com>
2022-10-01 13:51:08 +0600

    добавлено предасталения удления записи
    создан шаблон
    добавлен путь страницы

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
 from webapp.views.base import index_view
-from webapp.views.guest import guest_create_view
+from webapp.views.guest import guest_create_view, guest_delete_view
 
 
 urlpatterns= [
     path("", index_view, name='index'),
-    path('guest/create', guest_create_view, name='guest_create')
+    path('guest/create', guest_create_view, name='guest_create'),
+    path('guest/<int:pk>/delete', guest_delete_view, name='guest_delete')
 ]
--- source/webapp/views/guest.py
+++ source/webapp/views/guest.py
@@ -1,6 +1,6 @@
 
 from webapp.models import GuestBook
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from webapp.forms import AuthorForm
 
 
@@ -14,4 +14,12 @@ def guest_create_view(request, *args, **kwargs):
         return render(request, 'guest_create.html', context={'form': form})
     guest = GuestBook.objects.create(**form.cleaned_data)        
     if request.method =='POST':
+        return redirect('index')
+    
+def guest_delete_view(request, pk):
+    guest = get_object_or_404(GuestBook, pk=pk)
+    if request.method == 'GET':
+       return render(request, 'delete.html', context={'guest': guest})
+    elif request.method == 'POST':
+        guest.delete()
         return redirect('index')
